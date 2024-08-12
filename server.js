944d5ab89b9c3c3af09d35deb44fa48a1fa79feb
LeonardoLombana <leonardo.lc02@hotmail.com>
2024-08-11 22:49:48 -0500

    integraciòn con la DB

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
 
+
+const db = require("./app/models");
+const { connect } = require('mongoose');
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("DB connect succesfully");
+    })
+    .catch(err => {
+        console.log("DB connection not succesful", err);
+        process.exit();
+    });
+
+
 app.get("/", (req,res) => {
     res.json({message: "Hello there"})
 });

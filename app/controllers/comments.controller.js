240d2d1df2004b0ed7fda5a5fbd3f165993e3338
LeonardoLombana <leonardo.lc02@hotmail.com>
2024-08-11 23:10:44 -0500

    Se crea archivo comments.controller

---
+++ app/controllers/comments.controller.js
@@ -0,0 +1,32 @@
+const db = require("../models");
+const Comment = db.comments;
+
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update= (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
+};
+
+exports.deleteAll = (req, res) => {
+
+};
+
+exports.findAllPublished = (req, res) => {
+
+};
+
